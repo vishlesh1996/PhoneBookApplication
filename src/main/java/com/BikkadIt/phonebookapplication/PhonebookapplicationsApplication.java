ba40b4199cb0297780f3b6b6960adb19a5a2181c
vishlesh11996 <vishlesh.reakalwar@gmail.com>
2022-06-14 00:27:27 +0530

    PHN-2 : create spring boot project with required dependencies

---
+++ src/main/java/com/BikkadIt/phonebookapplication/PhonebookapplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.phonebookapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookapplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebookapplicationsApplication.class, args);
+	}
+
+}
